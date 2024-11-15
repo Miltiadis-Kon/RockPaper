5947cd34f09ec734f7b140520cd5a8b6db707864
Miltiadis-Kon <miltiadiskontos@gmail.com>
2024-11-15 14:06:44 +0200

    Add animations

--- Assets/GameMngr.cs
+++ Assets/GameMngr.cs
@@ -11,8 +11,33 @@ public class GameMngr : MonoBehaviour
         Scissors
     }
 
+    
+    public UIMananger uiManager;
 
     public string previousOption = "";
+
+
+    void Awake()
+    {
+        previousOption = "";
+        if (uiManager == null)
+        {
+            uiManager = GameObject.Find("UIManager").GetComponent<UIMananger>();
+        }
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(StartCountdownAndPlay());
+        }
+    }
+
+
     public void UpdateUserOption(string option)
     {
         if (option != previousOption)
@@ -34,31 +59,47 @@ public class GameMngr : MonoBehaviour
         if (previousOption=="") 
         {
             Debug.Log("Player choice is empty");
+            string[] options = { "Rock", "Paper", "Scissors" };
+            previousOption = options[Random.Range(0, 3)];
         }
-        else
-        {
-        cpuChoice = (Choice)Random.Range(0, 3);
 
+        cpuChoice = (Choice)Random.Range(0, 3);
         playerChoice = (Choice)System.Enum.Parse(typeof(Choice), previousOption);
         DetermineWinner();
-        }
+        previousOption = ""; // Reset the player choice 
         }
 
     private void DetermineWinner()
     {
         if (playerChoice == cpuChoice)
         {
-            Debug.Log("It's a tie!");
+            uiManager.ShowWinner("Draw");
         }
         else if ((playerChoice == Choice.Rock && cpuChoice == Choice.Scissors) ||
                  (playerChoice == Choice.Paper && cpuChoice == Choice.Rock) ||
                  (playerChoice == Choice.Scissors && cpuChoice == Choice.Paper))
         {
-            Debug.Log("Player wins!");
+            uiManager.ShowWinner("Player");
         }
         else
         {
-            Debug.Log("CPU wins!");
+            uiManager.ShowWinner("CPU");
         }
     }
+
+    public IEnumerator StartCountdownAndPlay()
+    {
+        int countdown = 3;
+        while (countdown > 0)
+        {
+            Debug.Log(countdown);
+            uiManager.animateCountdown(countdown);
+            yield return new WaitForSeconds(1);
+            countdown--;
+        }
+        uiManager.animateCountdown(countdown);
+        PlayGame();
+    }
+
+
 }
--- Assets/UIMananger.cs
+++ Assets/UIMananger.cs
@@ -21,6 +21,7 @@ public class UIMananger : MonoBehaviour
     public TMP_Text comOption;
     public TMP_Text comOptionDark;
 
+    public TMP_Text countdownTxt;
 
     // Test variables
     public int total_lives = 5;
@@ -51,14 +52,19 @@ public class UIMananger : MonoBehaviour
     {
         string _option = manager.previousOption;
         userOption.text = userOptionDark.text =  _option;
-        // Test lives
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            user_lives--;
-            UpdateLives(total_lives,user_lives , userLives);
-        }
 
     }
+
+    public void animateCountdown(int countdown)
+    {
+        if (countdown <= 0)
+        {
+            countdownTxt.text = "";
+        }
+        else
+        countdownTxt.text = countdown.ToString();
+    }
+
 
     private void SetupLives()
     {
@@ -66,12 +72,51 @@ public class UIMananger : MonoBehaviour
         UpdateLives(total_lives, cpu_lives, cpuLives);
     }
 
+    public void ShowWinner(string winner)
+    {
+        if (winner == "Player")
+        {
+            cpu_lives--;
+            UpdateLives(total_lives, cpu_lives, cpuLives);
+            LogWinner(1);
+        }
+        else if (winner == "CPU")
+        {
+            user_lives--;
+            UpdateLives(total_lives, user_lives, userLives);
+            LogWinner(-1);
+        }
+        else
+        {
+            LogWinner(0);
+        }
+    }
+
+    private void LogWinner(int winner)
+    {
+        switch (winner)
+        {
+            case 1:
+                countdownTxt.text = "You win!";
+                break;
+            case -1:
+                countdownTxt.text = "You lost!";
+                break;
+            case 0:
+                countdownTxt.text = "It's a tie!";
+                break;
+            case 2:
+                countdownTxt.text = "Game Over!";
+                break;
+        }
+    }
+
 
     private void UpdateLives(int total_lives,int lives,Transform parent)
     {
         if (lives< 0)
         {
-           print("Game Over");
+           LogWinner(2);
             // Game end
         }
         else
